da6d4a4c1143a9ae6283324fda3e708371f7ff72
Massimo Spadotto <massimo.spadotto@gmail.com>
2022-01-13 19:08:18 +0100

    emacs configuration completely redone

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,6 +1,3 @@
-
-;; My init.el
-
 ;; Setup package manager
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (require 'package)
@@ -10,23 +7,28 @@
 			 ("elpa" . "https://elpa.gnu.org/packages/")
 			 ("nongnu" . "https://elpa.nongnu.org/nongnu/")
 			 ))
-
 (package-initialize)
 
 ;; Bootstrap 'use-package'
-
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure 't)
 
 ;; Tell emacs where is your personal elisp lib dir
 (add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; Config file via org-babel
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
+ '(package-selected-packages
+   '(multiple-cursors magit company-bibtex deft org-roam-ui org-roam org-bullets olivetti org-superstar org-appear flycheck-clang-analyzer flycheck ivy-rich which-key use-package tron-legacy-theme smartparens rainbow-delimiters page-break-lines minimap linum-relative helpful doom-modeline diminish dashboard counsel-projectile company-tabnine beacon)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
