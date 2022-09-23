df43f46f38829166b4a88b1de49d230c67c1df47
Massimo Spadotto <massimo.spadotto@gmail.com>
2022-09-23 23:21:52 +0200

    added pocket-read

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -29,9 +29,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "cf9414f229f6df728eb2a5a9420d760673cca404fee9910551caf9c91cff3bfa" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
+ '(ispell-dictionary nil)
  '(minimap-mode nil)
  '(package-selected-packages
-   '(pdf-tools doom-themes all-the-icons-dired all-the-icons-ivy yasnippet-snippets yasnippet try ace-window avy undo-tree multiple-cursors magit company-bibtex deft org-roam-ui org-roam org-bullets olivetti org-superstar org-appear flycheck-clang-analyzer flycheck ivy-rich which-key use-package tron-legacy-theme smartparens rainbow-delimiters page-break-lines minimap linum-relative helpful doom-modeline diminish dashboard counsel-projectile company-tabnine beacon)))
+   '(pocket-reader pdf-tools doom-themes all-the-icons-dired all-the-icons-ivy yasnippet-snippets yasnippet try ace-window avy undo-tree multiple-cursors magit company-bibtex deft org-roam-ui org-roam org-bullets olivetti org-superstar org-appear flycheck-clang-analyzer flycheck ivy-rich which-key use-package tron-legacy-theme smartparens rainbow-delimiters page-break-lines minimap linum-relative helpful doom-modeline diminish dashboard counsel-projectile company-tabnine beacon)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
