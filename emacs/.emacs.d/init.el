c31b81b80f6ef904b021beea9a254051a8af9437
dwarf78 <massimo.spadotto@gmail.com>
2023-02-08 22:23:36 +0100

    added elfeed for RSS reading

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,4 @@
-;; Setup package manager
+;;; Setup package manager
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -9,17 +9,17 @@
 			 ))
 (package-initialize)
 
-;; Bootstrap 'use-package'
+;;; Bootstrap 'use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure 't)
 
-;; Tell emacs where is your personal elisp lib dir
+;;; Tell emacs where is your personal elisp lib dir
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
-;; Config file via org-babel
+;;; Config file via org-babel
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 (custom-set-variables
@@ -27,14 +27,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "cf9414f229f6df728eb2a5a9420d760673cca404fee9910551caf9c91cff3bfa" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
- '(gdb-many-windows t)
- '(gdb-show-main t)
- '(ispell-dictionary nil)
- '(minimap-mode nil)
- '(package-selected-packages
-   '(pocket-reader pdf-tools doom-themes all-the-icons-dired all-the-icons-ivy yasnippet-snippets yasnippet try ace-window avy undo-tree multiple-cursors magit company-bibtex deft org-roam-ui org-roam org-bullets olivetti org-superstar org-appear flycheck-clang-analyzer flycheck ivy-rich which-key use-package tron-legacy-theme smartparens rainbow-delimiters page-break-lines minimap linum-relative helpful doom-modeline diminish dashboard counsel-projectile company-tabnine beacon)))
+ '(elfeed-feeds
+   '("https://www.glistatigenerali.com/feed/" "https://www.theatlantic.com/feed/all/" "http://www.limesonline.com/feed" "https://c-for-dummies.com/blog/?feed=rss2" "https://johnhcochrane.blogspot.com/feeds/posts/default?alt=rss" "https://aswathdamodaran.blogspot.com/feeds/posts/default?alt=rss" "https://clopremium.co.uk/feed/")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
