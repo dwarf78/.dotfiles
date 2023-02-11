7a6b83df8dc3bbf978924cd04012dae412ef8c49
dwarf78 <massimo.spadotto@gmail.com>
2023-02-11 18:21:25 +0100

    modified init.el to avoid been prompted to follow symlink

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -16,12 +16,16 @@
 (require 'use-package)
 (setq use-package-always-ensure 't)
 
+;; Avoid been prompted to follow link
+(setq vc-follow-symlinks t)
+
 ;;; Tell emacs where is your personal elisp lib dir
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ;;; Config file via org-babel
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -30,7 +34,7 @@
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(elfeed-org cmake-mode yasnippet-snippets which-key use-package undo-tree try tron-legacy-theme smartparens rainbow-delimiters pocket-reader page-break-lines org-superstar org-roam-ui org-bullets org-appear olivetti multiple-cursors minimap magit linum-relative ivy-rich helpful flycheck-clang-analyzer elfeed doom-themes doom-modeline diminish deft dashboard counsel-projectile company-tabnine company-bibtex beacon all-the-icons-ivy all-the-icons-dired ace-window)))
+   '(memoize unicode-fonts elfeed-org cmake-mode yasnippet-snippets which-key use-package undo-tree try tron-legacy-theme smartparens rainbow-delimiters pocket-reader page-break-lines org-superstar org-roam-ui org-bullets org-appear olivetti multiple-cursors minimap magit linum-relative ivy-rich helpful flycheck-clang-analyzer elfeed doom-themes doom-modeline diminish deft dashboard counsel-projectile company-tabnine company-bibtex beacon all-the-icons-ivy all-the-icons-dired ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
