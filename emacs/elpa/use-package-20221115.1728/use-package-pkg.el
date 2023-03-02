b43362b850eed7dbbc12806f1e328e16ca1abe8e
dwarf78 <massimo.spadotto@gmail.com>
2023-03-02 01:04:49 +0100

    cleaned cache

---
+++ emacs/elpa/use-package-20221115.1728/use-package-pkg.el
@@ -0,0 +1,13 @@
+(define-package "use-package" "20221115.1728" "A configuration macro for simplifying your .emacs"
+  '((emacs "24.3")
+    (bind-key "2.4"))
+  :commit "09f210c5989454fa2c8729f4c0788c1d18c0ce22" :authors
+  '(("John Wiegley" . "johnw@newartisans.com"))
+  :maintainer
+  '("John Wiegley" . "johnw@newartisans.com")
+  :keywords
+  '("dotemacs" "startup" "speed" "config" "package")
+  :url "https://github.com/jwiegley/use-package")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
